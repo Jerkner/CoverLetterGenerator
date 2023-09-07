3034cccc68b1e529cadf993cff4f3a23c7826e13
Jerkner <philip@jerkner.se>
2023-09-07 20:26:47 +0200

    added select options

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
     const submitButton = document.getElementById("submit");
     const copyButton = document.getElementById("copyButton");
+    const downloadButton = document.getElementById("downloadPDF");
     const outputElement = document.getElementById("output");
-  
+    const today = new Date().toLocaleDateString('en-GB');
+
     submitButton.addEventListener("click", function (event) {
-      event.preventDefault();
-  
-      const name = document.getElementById("name").value;
-      const recruitingPage = document.getElementById("recruitingPage").value;
-  
-      const template = `Dear ${name},\n\nI am writing to express my strong interest in the Front-End Developer position at ${name}, as advertised on ${recruitingPage}. With a passion for creating engaging and user-friendly web experiences, I am excited about the opportunity to contribute my skills to your team.\n\nAs a self-taught Front-End Developer, I have honed my expertise in HTML, CSS, JavaScript, and React through practical projects and continuous learning. I am skilled in crafting visually appealing and responsive websites that enhance user interaction and engagement. In addition to my technical proficiency, my keen eye for design and user experience allows me to translate complex concepts into intuitive and seamless interfaces.\n\nWhat sets me apart is my ability to collaborate effectively in cross-functional teams. I thrive in dynamic environments where I can contribute my creativity and problem-solving skills to achieve shared goals. My experience in mentorship and assisting fellow team members in previous roles has allowed me to develop strong communication and teamwork skills that are essential for a collaborative development environment.\n\nThank you for considering my application. I look forward to the opportunity to discuss how my background and enthusiasm for web development can contribute to the success of ${name}.\n\Please find my resume at https://jerkner-resume.netlify.app/\n\My portfolio at https://jerkner-portfolio.netlify.app/\n\My GitHub at https://github.com/Jerkner\n\nBest regards,\nPhilip Jerkner`;
-  
-      outputElement.innerText = template;
-      document.getElementById("name").value = '';
-      document.getElementById("recruitingPage").value = '';
+        event.preventDefault();
+    
+        const name = document.getElementById("name").value;
+        const role = document.getElementById("role").value;
+        const recruitingPage = document.getElementById("recruitingPage").value;
+    
+        const template = `
+Name: Philip Jerkner
+Location: Sweden and Portugal
+Email: philip@jerkner.se
+Phone: +46 76 808 53 92
+
+${today}
+
+Dear hiring team at ${name},
+
+I am writing to express my strong interest in the ${role} position at ${name}, as advertised on ${recruitingPage}. As a self-taught front-end development with a strong passion for design and UI/UX, I am eager to contribute my skills to your dynamic development team.
+
+Proficient in HTML5, CSS3, JavaScript, and React, I'm adept at creating clean and intuitive interfaces. My experience in API integration and version control with Git complements my technical proficiency.
+
+I am enthusiastic about the opportunity to bring my skills and passion for development to ${name}. If you seek a developer who excels at transforming complexities into elegant solutions, I would welcome the chance to discuss further.
+
+Thank you for considering my application. I look forward to the possibility of contributing to the success of ${name}.
+
+Resume: https://jerkner-resume.netlify.app/
+Portfolio: https://jerkner-portfolio.netlify.app/
+GitHub: https://github.com/Jerkner
+
+Best regards,
+Philip Jerkner`;
+        
+        outputElement.value = template;
     });
-  
+    
+
     copyButton.addEventListener("click", function () {
-      const textToCopy = outputElement.innerText;
-  
-      const textArea = document.createElement("textarea");
-      textArea.value = textToCopy;
-      document.body.appendChild(textArea);
-      textArea.select();
-      document.execCommand("copy");
-      document.body.removeChild(textArea);
-  
+        outputElement.select();
+        document.execCommand("copy");
+        outputElement.blur();
     });
-  });
-  
+
+    downloadButton.addEventListener("click", function () {
+        const content = outputElement.value;
+    
+        const docDefinition = {
+            content: content
+        };
+    
+        pdfMake.createPdf(docDefinition).download('Philip_Jerkner_Cover_Letter.pdf');
+    });
+    
+    
+});
