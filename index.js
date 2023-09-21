18d94fcd8efd0068ae47e2262679af4c7da44d59
Jerkner <philip@jerkner.se>
2023-09-21 14:06:23 +0100

    Prettier update

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const submitButton = document.getElementById("submit");
-    const copyButton = document.getElementById("copyButton");
-    const downloadButton = document.getElementById("downloadPDF");
-    const outputElement = document.getElementById("output");
-    const today = new Date().toLocaleDateString('en-GB');
+    const submitButton = document.getElementById("submit")
+    const copyButton = document.getElementById("copyButton")
+    const downloadButton = document.getElementById("downloadPDF")
+    const outputElement = document.getElementById("output")
+    const today = new Date().toLocaleDateString("en-GB")
 
     submitButton.addEventListener("click", function (event) {
-        event.preventDefault();
-    
-        const name = document.getElementById("name").value;
-        const role = document.getElementById("role").value;
-        const recruitingPage = document.getElementById("recruitingPage").value;
-        const language = document.getElementById("language").value;
-    
-        const template = language === "English" ?
-`Name: Philip Jerkner
+        event.preventDefault()
+
+        const name = document.getElementById("name").value
+        const role = document.getElementById("role").value
+        const recruitingPage = document.getElementById("recruitingPage").value
+        const language = document.getElementById("language").value
+
+        const template =
+            language === "English"
+                ? `Name: Philip Jerkner
 Location: Portugal and Sweden
 Email: philip@jerkner.se
 Phone: +46 76 808 53 92
@@ -23,7 +24,13 @@ ${today}
 
 Dear hiring team at ${name},
 
-I am writing to express my strong interest in the ${role} position at ${name}, as advertised on ${recruitingPage}. As a self-taught ${role === "React Developer" ? "React developer" : role === "JavaScript Developer" ? "JavaScript developer" : role.toLowerCase()} with a strong passion for design and UI/UX, I am eager to contribute my skills to your development team.
+I am writing to express my strong interest in the ${role} position at ${name}, as advertised on ${recruitingPage}. As a self-taught ${
+                      role === "React Developer"
+                          ? "React developer"
+                          : role === "JavaScript Developer"
+                          ? "JavaScript developer"
+                          : role.toLowerCase()
+                  } with a strong passion for design and UI/UX, I am eager to contribute my skills to your development team.
 
 Proficient in HTML5, CSS3, JavaScript, and React, I'm adept at creating clean and intuitive interfaces. My experience in API integration and version control with Git complements my technical proficiency.
 
@@ -36,9 +43,8 @@ Portfolio: https://jerkner-portfolio.netlify.app/
 GitHub: https://github.com/Jerkner
 
 Best regards,
-Philip Jerkner` :
-
-`Namn: Philip Jerkner
+Philip Jerkner`
+                : `Namn: Philip Jerkner
 Plats: Portugal och Sverige
 E-post: philip@jerkner.se
 Telefon: +46 76 808 53 92
@@ -47,7 +53,23 @@ ${today}
 
 Till rekryteringsteamet på ${name},
 
-Jag skriver för att uttrycka mitt starka intresse för ${role === "React Developer" ? "React-utvecklare" : role === "JavaScript Developer" ? "JavaScript-utvecklare" : role === "Web Developer" ? "webbutvecklare" : "frontend-utvecklare"}-positionen på ${name}, som annonserades på ${recruitingPage}. Som en självlärd ${role === "React Developer" ? "React-utvecklare" : role === "JavaScript Developer" ? "JavaScript-utvecklare" : role === "Web Developer" ? "webbutvecklare" : "frontend-utvecklare"} med en passion för design och användarupplevelse ser jag fram emot att få bidra med mina färdigheter till ert utvecklingsteam.
+Jag skriver för att uttrycka mitt starka intresse för ${
+                      role === "React Developer"
+                          ? "React-utvecklare"
+                          : role === "JavaScript Developer"
+                          ? "JavaScript-utvecklare"
+                          : role === "Web Developer"
+                          ? "webbutvecklare"
+                          : "frontend-utvecklare"
+                  }-positionen på ${name}, som annonserades på ${recruitingPage}. Som en självlärd ${
+                      role === "React Developer"
+                          ? "React-utvecklare"
+                          : role === "JavaScript Developer"
+                          ? "JavaScript-utvecklare"
+                          : role === "Web Developer"
+                          ? "webbutvecklare"
+                          : "frontend-utvecklare"
+                  } med en passion för design och användarupplevelse ser jag fram emot att få bidra med mina färdigheter till ert utvecklingsteam.
 
 Jag behärskar HTML5, CSS3, JavaScript och React och är skicklig på att skapa rena och intuitiva gränssnitt. Min erfarenhet av API-integration och versionshantering med Git kompletterar min tekniska kompetens.
 
@@ -62,26 +84,25 @@ GitHub: https://github.com/Jerkner
 Vänliga hälsningar,
 Philip Jerkner`
 
-        outputElement.value = template;
-        document.getElementById("name").value = ''
-    });
-    
+        outputElement.value = template
+        document.getElementById("name").value = ""
+    })
 
     copyButton.addEventListener("click", function () {
-        outputElement.select();
-        document.execCommand("copy");
-        outputElement.blur();
-    });
+        outputElement.select()
+        document.execCommand("copy")
+        outputElement.blur()
+    })
 
     downloadButton.addEventListener("click", function () {
-        const content = outputElement.value;
-    
+        const content = outputElement.value
+
         const docDefinition = {
             content: content
-        };
-    
-        pdfMake.createPdf(docDefinition).download('Philip_Jerkner_Cover_Letter.pdf');
-    });
-    
-    
-});
+        }
+
+        pdfMake
+            .createPdf(docDefinition)
+            .download("Philip_Jerkner_Cover_Letter.pdf")
+    })
+})
